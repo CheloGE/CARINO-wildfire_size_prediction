1dd442223c1a44fac32e8d38437c4e3cee067043
Marcelo García <chelo_Ge@hotmail.com>
2020-08-22 00:30:49 -0500

    score: 37

--- helpers.py
+++ helpers.py
@@ -9,7 +9,7 @@ from IPython import get_ipython
 get_ipython().run_line_magic('matplotlib', 'inline')
 ###########################################
 
-import matplotlib.pyplot as pl
+import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import numpy as np
 import pandas as pd
@@ -27,23 +27,23 @@ def feature_plot(importances, X_train, y_train, top_k=5):
     values = importances[indices][:top_k]
 
     # Creat the plot
-    fig = pl.figure(figsize = (15,20))
-    pl.title(f"Normalized Weights for First {top_k} Most Predictive Features", fontsize = 16)
-    #pl.bar(np.arange(top_k), values, width = 0.6, align="center", color = '#00A000', \
+    fig = plt.figure(figsize = (15,20))
+    plt.title(f"Normalized Weights for First {top_k} Most Predictive Features", fontsize = 16)
+    #plt.bar(np.arange(top_k), values, width = 0.6, align="center", color = '#00A000', \
     #      label = "Feature Weight")
-    pl.barh(np.arange(top_k), values[::-1], align="center", height=0.4, label = "Feature Weight")
-    #pl.bar(np.arange(top_k) - 0.3, np.cumsum(values), width = 0.2, align = "center", color = '#00A0A0', \
+    plt.barh(np.arange(top_k), values[::-1], align="center", height=0.4, label = "Feature Weight")
+    #plt.bar(np.arange(top_k) - 0.3, np.cumsum(values), width = 0.2, align = "center", color = '#00A0A0', \
     #      label = "Cumulative Feature Weight")
-    pl.barh(np.arange(top_k) - 0.3, np.cumsum(values)[::-1], height=0.4, align="center", label = "Cumulative Feature Weight") 
+    plt.barh(np.arange(top_k) - 0.3, np.cumsum(values)[::-1], height=0.4, align="center", label = "Cumulative Feature Weight") 
             
-    pl.yticks(np.arange(top_k), columns[::-1])
-    pl.ylim((-0.5, top_k-.5))
-    pl.xlabel("Weight", fontsize = 12)
-    pl.ylabel("Feature", fontsize = 12)
+    plt.yticks(np.arange(top_k), columns[::-1])
+    plt.ylim((-0.5, top_k-.5))
+    plt.xlabel("Weight", fontsize = 12)
+    plt.ylabel("Feature", fontsize = 12)
     
-    pl.legend(loc = 'upper right')
-    pl.tight_layout()
-    pl.show()
+    plt.legend(loc = 'upper right')
+    plt.tight_layout()
+    plt.show()
     
     return pd.DataFrame({'Features':columns, 'Importance value':values})
 
@@ -68,12 +68,13 @@ def datetime_feature_engineering(df_temp, inplace = False):
     datetime_features = df_temp.columns[df_temp.dtypes=='<M8[ns]']
     df = df_temp.copy(deep=True)
     df['fire_duration'] = (df['ex_fs_date']-df['fire_start_date']).astype('timedelta64[m]')
-    #df['time_to_ex'] = (df['ex_fs_date']-df['bh_fs_date']).astype('timedelta64[m]')
-    #df['time_to_uc'] = (df['uc_fs_date']-df['fire_fighting_start_date']).astype('timedelta64[h]')
-    #df['time_to_bh'] = (df['bh_fs_date']-df['fire_fighting_start_date']).astype('timedelta64[h]')
-    df['fire_fight_response_time'] = (df['fire_fighting_start_date']-df['discovered_date']).astype('timedelta64[m]')
+    df['time_to_ex'] = (df['ex_fs_date']-df['bh_fs_date']).astype('timedelta64[m]')
+    df['time_to_uc'] = (df['uc_fs_date']-df['bh_fs_date']).astype('timedelta64[m]')
+    df['time_to_bh'] = (df['bh_fs_date']-df['fire_fighting_start_date']).astype('timedelta64[m]')
+    #df['fire_fight_response_time'] = (df['fire_fighting_start_date']-df['discovered_date']).astype('timedelta64[m]')
     df['time_to_discover'] = (df['discovered_date']-df['fire_start_date']).astype('timedelta64[m]')
-    #df['time_to_report'] = (df['reported_date']-df['discovered_date']).astype('timedelta64[h]')
+    df['time_to_report'] = (df['reported_date']-df['discovered_date']).astype('timedelta64[m]')
+    #df['time_to_assess'] = (df['start_for_fire_date']-df['discovered_date']).astype('timedelta64[m]')
     
     return df.drop(datetime_features, 1, inplace=inplace)
 
@@ -141,3 +142,43 @@ def load_model(path):
     """
     with open(path, 'rb') as f:
         return pickle.load(f)
+    
+def fire_number_feature_engineering(df_input):
+    df = df_input.copy(deep=True)
+    df['Location'] = df['fire_number'].apply(lambda x: x[0])
+    df['wildfires_in_location'] = df['fire_number'].apply(lambda x: int(x[-3:]))
+    return df
+
+def get_df_by_size_class(df_input, size_class):
+    df = df_input.copy(deep=True)
+    df = fire_number_feature_engineering(df)
+    df.drop(['fire_number'], 1, inplace=True)
+    size_class_indx = df['size_class']==size_class
+    non_date_columns_indx = ['date' not in column for column in df.columns]
+    non_date_columns = df.columns[non_date_columns_indx]
+    df_size_class = df[non_date_columns][size_class_indx]
+    return df_size_class
+
+def data_analysis_by_size_class(df_input):
+    df = df_input.copy(deep=True)
+    classes = df['size_class'].unique()
+    df_array = [get_df_by_size_class(df, curr_class) for curr_class in classes]
+    for feature in df_array[0].columns:
+        feature_pd_series = [curr_df[feature] for curr_df in df_array]
+        if feature_pd_series[0].dtype=='O':
+            plt.figure(figsize=(15,20))
+            for i in range(len(feature_pd_series)):
+                plt.subplot(int(511+i))
+                plt.title(feature+'\n class : '+classes[i])
+                if feature == 'det_agent':
+                    feature_pd_series[i].value_counts()[:20].plot(kind='barh');
+                else:
+                    feature_pd_series[i].value_counts().plot(kind='barh');
+        else:
+            plt.figure(figsize=(15,7))
+            for i in range(len(feature_pd_series)):
+                plt.subplot(int(511+i))
+                plt.title(feature+'\n class : '+classes[i])
+                plt.boxplot(feature_pd_series[i], vert=False); 
+            
+    return df_array
